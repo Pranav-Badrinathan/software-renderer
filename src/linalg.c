705dc39f6d9f40d60a05b6c2415cdd376962f642
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2023-11-13 01:21:24 -0500

    Added rotation matricies, and matrix-matrix multiplication

--- src/linalg.c
+++ src/linalg.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <stdio.h>
 
+// VECTOR OPERATIONS
+
 struct Vec4 vector_add(struct Vec4 *v1, struct Vec4 *v2) {
 	return (struct Vec4) {
 		v1->x + v2->x,
@@ -55,13 +57,51 @@ struct Vec4 vector_norm(struct Vec4 *v) {
 	return vector_div(v, vector_mag(v));
 }
 
-// MATRIX STUFFS
+// MATRIX OPERATIONS 
+
+struct Vec4 matrix_vec3_mul(struct Mat4x4 *mat, struct Vec4 *v) {
+	struct Vec4 point = {0};
+
+	point.x = v->x * mat->v[0][0] + v->y * mat->v[1][0] + v->z * mat->v[2][0] + v->w * mat->v[3][0];
+	point.y = v->x * mat->v[0][1] + v->y * mat->v[1][1] + v->z * mat->v[2][1] + v->w * mat->v[3][1];
+	point.z = v->x * mat->v[0][2] + v->y * mat->v[1][2] + v->z * mat->v[2][2] + v->w * mat->v[3][2];
+	point.w = v->x * mat->v[0][3] + v->y * mat->v[1][3] + v->z * mat->v[2][3] + v->w * mat->v[3][3];
+
+	return point;
+}
+
+struct Mat4x4 matrix_transpose(struct Mat4x4 *mat) {
+	struct Mat4x4 ret = {0};
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			ret.v[j][i] = mat->v[i][j];
+		}
+	}
+
+	return ret;
+}
+
+struct Mat4x4 matrix_matrix_mul(struct Mat4x4 *mat1, struct Mat4x4 *mat2) {
+	struct Mat4x4 ret = {0};
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			for (int k = 0; k < 4; k++) {
+				ret.v[i][j] += mat1->v[i][k] * mat2->v[k][j];
+			}
+		}
+	}
+
+	return ret;
+}
+
+// PREDEFINED MATRICIES
 
 struct Mat4x4 get_proj_matrix(const float near, 
 							  const float far, 
 							  const float fov, 
 							  const float aspect_ratio) {
-
 	// Initialize matrix to 0;
 	struct Mat4x4 mat = {0};
 
@@ -77,13 +117,32 @@ struct Mat4x4 get_proj_matrix(const float near,
 	return mat;
 }
 
-struct Vec4 matrix_vec3_mul(struct Mat4x4 *mat, struct Vec4 *v) {
-	struct Vec4 point = {0};
+struct Mat4x4 get_xrot_matrix(const float angle) {
+	struct Mat4x4 mat = {0};
+	mat.v[1][1] = cosf(angle);
+	mat.v[2][2] = cosf(angle);
+	mat.v[2][1] = -sinf(angle);
+	mat.v[1][2] = sinf(angle);
 
-	point.x = v->x * mat->v[0][0] + v->y * mat->v[1][0] + v->z * mat->v[2][0] + v->w * mat->v[3][0];
-	point.y = v->x * mat->v[0][1] + v->y * mat->v[1][1] + v->z * mat->v[2][1] + v->w * mat->v[3][1];
-	point.z = v->x * mat->v[0][2] + v->y * mat->v[1][2] + v->z * mat->v[2][2] + v->w * mat->v[3][2];
-	point.w = v->x * mat->v[0][3] + v->y * mat->v[1][3] + v->z * mat->v[2][3] + v->w * mat->v[3][3];
+	return mat;
+}
 
-	return point;
+struct Mat4x4 get_yrot_matrix(const float angle) {
+	struct Mat4x4 mat = {0};
+	mat.v[0][0] = cosf(angle);
+	mat.v[2][2] = cosf(angle);
+	mat.v[0][2] = -sinf(angle);
+	mat.v[2][0] = sinf(angle);
+
+	return mat;
+}
+
+struct Mat4x4 get_zrot_matrix(const float angle) {
+	struct Mat4x4 mat = {0};
+	mat.v[0][0] = cosf(angle);
+	mat.v[1][1] = cosf(angle);
+	mat.v[1][0] = -sinf(angle);
+	mat.v[0][1] = sinf(angle);
+
+	return mat;
 }
